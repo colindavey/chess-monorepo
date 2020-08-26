e284003d3324db5d39fcc69c987219cc0fd0f3c6
Colin Davey <himself@colindavey.com>
2020-08-26 14:25:40 -0700

    Detect and display draw (tie) status

--- packages/react-tic-tac-toe-to-chess/index.js
+++ packages/react-tic-tac-toe-to-chess/index.js
@@ -104,6 +104,8 @@ class Game extends React.Component {
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
+        } else if (this.state.stepNumber === 9) {
+            status = "Draw";
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
