8839d493200aaa270269a899609a51ed47098284
Colin Davey <himself@colindavey.com>
2020-08-26 13:38:17 -0700

    Render board with two loops instead of hardcoded

--- packages/react-tic-tac-toe-to-chess/index.js
+++ packages/react-tic-tac-toe-to-chess/index.js
@@ -21,23 +21,16 @@ class Board extends React.Component {
     }
 
     render() {
+        let element = [];
+        for (let i=0; i < 3; i++) {
+            element.push(<div className="board-row"></div>)
+            for (let j=0; j < 3; j++) {
+                element.push(this.renderSquare((3*i) + j))
+            }
+        }
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {element}
             </div>
         );
     }
