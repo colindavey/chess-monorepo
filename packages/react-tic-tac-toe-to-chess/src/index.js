d02a667aad831effb1724192a92410b6694d7ade
Colin Davey <himself@colindavey.com>
2021-02-07 23:50:57 -0800

    Lost tic-tac-toe winner logic, added highlight of clicked square

--- packages/react-tic-tac-toe-to-chess/src/index.js
+++ packages/react-tic-tac-toe-to-chess/src/index.js
@@ -6,17 +6,42 @@ const dims = 8;
 // const dims = 5;
 
 const WHITE_KING = "\u2654";
-// const WHITE_QUEEN = "\u2655";
-// const WHITE_ROOK = "\u2656";
-// const WHITE_BISHOP = "\u2657";
-// const WHITE_KNIGHT = "\u2658";
-// const WHITE_PAWN = "\u2659";
+const WHITE_QUEEN = "\u2655";
+const WHITE_ROOK = "\u2656";
+const WHITE_BISHOP = "\u2657";
+const WHITE_KNIGHT = "\u2658";
+const WHITE_PAWN = "\u2659";
 const BLACK_KING = "\u265A";
-// const BLACK_QUEEN = "\u265B";
-// const BLACK_ROOK = "\u265C";
-// const BLACK_BISHOP = "\u265D";
-// const BLACK_KNIGHT = "\u265E";
-// const BLACK_PAWN = "\u265F";
+const BLACK_QUEEN = "\u265B";
+const BLACK_ROOK = "\u265C";
+const BLACK_BISHOP = "\u265D";
+const BLACK_KNIGHT = "\u265E";
+const BLACK_PAWN = "\u265F";
+const pieceLookup = {
+    K: WHITE_KING,
+    Q: WHITE_QUEEN,
+    R: WHITE_ROOK,
+    B: WHITE_BISHOP,
+    N: WHITE_KNIGHT,
+    P: WHITE_PAWN,
+    k: BLACK_KING,
+    q: BLACK_QUEEN,
+    r: BLACK_ROOK,
+    b: BLACK_BISHOP,
+    n: BLACK_KNIGHT,
+    p: BLACK_PAWN,
+}
+const initPosition = 
+[
+    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+]
 
 const Square = ({onClick, value, highlighted, colorClass}) => {
     const highlightClass = highlighted ? "square-highlighted" : '';
@@ -32,27 +57,12 @@ const Square = ({onClick, value, highlighted, colorClass}) => {
     ); 
 }
 
-const Board = ({squares, reverse, onClick}) => {
+const Board = ({squares, reverse, onClick, clickedSquare}) => {
     const renderSquare = (i, j) => {
-        const value = squares[i][j]
-        const winner = calculateWinner(dims, squares);
-        // line is one of 
-        //   cN (or other number) - column N
-        //   rN (or other number) - row N
-        //   diagUp upper diagonal
-        //   diagDown lower diagonal
-        let highlighted;
-        if (winner) {
-            console.log(winner.line)
-            if (winner.line[0] === 'c' && parseInt(winner.line[1]) === j) {
-                highlighted = true;
-            } else if (winner.line[0] === 'r' && parseInt(winner.line[1]) === i) {
-                highlighted = true;
-            } else if (winner.line === 'diagUp' && i === j) {
-                highlighted = true;
-            } else if (winner.line === 'diagDown' && (i === ((dims-1) - j))) {
-                highlighted = true;
-            }
+        const value = pieceLookup[squares[i][j]];
+        let highlighted = false;
+        if (clickedSquare) {
+            highlighted = i === clickedSquare[0] && j === clickedSquare[1];
         }
         const colorClass = (i % 2 === j % 2) ? "square-black" : "square-white"
 
@@ -95,13 +105,16 @@ const Game  = () => {
     const [reverse, setReverse] = useState(false);
     const [history, setHistory] = useState(
         [{
-            squares: init2DimArray(dims),
+            // squares: init2DimArray(dims),
+            squares: initPosition,
             row: '',
             col: '',
         }])
     const [currentMoveNum, setCurrentMoveNum] = useState(0);
+    const [clickedSquare, setClickedSquare] = useState(null)
 
     const handleClick = (i, j) => {
+        setClickedSquare([i, j])
         let local_history = history.slice(0, currentMoveNum+1);
         const snapshot = local_history[local_history.length - 1];
         // Makes deep copy
@@ -109,9 +122,9 @@ const Game  = () => {
             return arr.slice();
         });
 
-        if (calculateWinner(dims, squares) || squares[i][j]) {
-            return;
-        }
+        // if (calculateWinner(dims, squares) || squares[i][j]) {
+        //     return;
+        // }
         squares[i][j] = moveNum2Letter(currentMoveNum);
         local_history.push({
             squares: squares,
@@ -140,15 +153,17 @@ const Game  = () => {
         //     listingButtons.reverse();
         // }
 
-        const winner = calculateWinner(dims, history[currentMoveNum].squares);
+        // const winner = calculateWinner(dims, history[currentMoveNum].squares);
+        // let status;
+        // if (winner) {
+        //     status = 'Winner: ' + winner.winner;
+        // } else if (currentMoveNum === dims*dims) {
+        //     status = "Draw";
+        // } else {
+        //     status = 'Next player: ' + moveNum2Letter(currentMoveNum);
+        // }
         let status;
-        if (winner) {
-            status = 'Winner: ' + winner.winner;
-        } else if (currentMoveNum === dims*dims) {
-            status = "Draw";
-        } else {
-            status = 'Next player: ' + moveNum2Letter(currentMoveNum);
-        }
+        status = 'Next player: ' + moveNum2Letter(currentMoveNum);
 
         return (
             <div>
@@ -170,6 +185,7 @@ const Game  = () => {
                     squares={history[currentMoveNum].squares}
                     onClick={(i, j) => handleClick(i, j)}
                     reverse={reverse}
+                    clickedSquare={clickedSquare}
                 />
             </div>
             <div className="game-info">
@@ -189,68 +205,20 @@ ReactDOM.render(
 function moveNum2Letter(moveNum) {
     const xIsNext = ((moveNum % 2) === 0)
     // return xIsNext ? 'X' : 'O';
-    return xIsNext ? WHITE_KING : BLACK_KING;
+    // return xIsNext ? WHITE_KING : BLACK_KING;
+    return xIsNext ? 'K' : 'k';
 }
 
 /* 2D functions */
-
-// line is one of 
-//   cN (or other number) - column N
-//   rN (or other number) - row N
-//   diagUp upper diagonal
-//   diagDown lower diagonal
-function calculateWinner(nDims, squares) {
-    const vectors = [];
-    const lines = [];
-    const diagVectorAscending = []
-    const diagVectorDescending = []
-    // Simultaneously build up all the row and column vectors as well as
-    // each diagonal vector
-    for (let ind=0; ind < nDims; ind++) {
-        const rowVector = squares[ind];
-        const colVector = squares.reduce(
-            (accum, vecIn) => {
-                accum.push(vecIn[ind])
-                return accum;
-            },
-            []
-        );
-        vectors.push(rowVector);
-        lines.push(`r${ind}`)
-        vectors.push(colVector);
-        lines.push(`c${ind}`)
-        diagVectorAscending.push(squares[ind][ind])
-        diagVectorDescending.push(squares[ind][(nDims-1)-ind])
-    }
-    vectors.push(diagVectorDescending)
-    lines.push('diagDown')
-    vectors.push(diagVectorAscending)
-    lines.push('diagUp')
-    // Run through all the vectors accumulated above, and see if we have a winner
-    for (let i = 0; i < vectors.length; i++) {
-        const vector = vectors[i]
-        const val = vector[0]
-        if (val) {
-            // Reduce results in true if all elements in the vector are the same
-            const haveWinner = vector.reduce(
-                (accum, valIn) => accum && (valIn === val)
-            )
-            if (haveWinner) {
-                return {winner : val, line : lines[i]};
-            }
-        }
-    }
-    return null;
-}
 
 function rowCol2key(nDims, row, col) {
     return (nDims * row) + col
 }
 
-function init2DimArray(nDims) {
-    const array2D = []
-    for (let row=0; row < nDims; row++) {
-        array2D.push(Array(nDims).fill(null));
-    }
-    return array2D;
-}
+// function init2DimArray(nDims) {
+//     const array2D = []
+//     for (let row=0; row < nDims; row++) {
+//         array2D.push(Array(nDims).fill(null));
+//     }
+//     return array2D;
+// }
