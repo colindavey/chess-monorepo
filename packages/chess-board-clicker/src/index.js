e05e1b4604ba050025249c2ac09723029bc7ad57
Colin Davey <himself@colindavey.com>
2021-07-09 16:45:19 -0700

    Added BoardClicker app

---
+++ packages/chess-board-clicker/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import Game from "./game"
+import { DumbBoard } from 'components'
+import * as chessApi from 'components' 
+
+const BoardClicker = () => {
+    const handleClick = () => console.log('click')
+    const highlightList = []
+    const position = chessApi.emptyPosition
+    return (
+        <DumbBoard
+        position={position}
+        highlightList={highlightList}
+        handleClick={handleClick}
+        />
+    )
+}
+
+ReactDOM.render(
+    <BoardClicker/>,
+    document.getElementById('root')
+);
+
