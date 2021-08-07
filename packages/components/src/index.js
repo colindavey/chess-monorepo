7323dacdcf983f6e2818ad63255e371940a367e1
Colin Davey <himself@colindavey.com>
2021-08-07 11:35:03 -0700

    PositionSetup now a component in components

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -3,6 +3,7 @@ import styles from './styles.module.css'
 
 export { default as DumbBoard } from './dumbBoard.js'
 export { default as Game } from './game.js'
+export { default as PositionSetup } from './setPosition.js'
 
 export * from './chessApi.js'
 export * from './chessUtils.js'
