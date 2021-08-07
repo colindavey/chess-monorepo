e9628119720974f8f02ff1ae53d904286898f96f
Colin Davey <himself@colindavey.com>
2021-08-07 13:08:27 -0700

    Added router app with links to game and setup

---
+++ packages/router-app/src/index copy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import './index.css';
+import {PositionSetup} from 'components'
+
+ReactDOM.render(
+    <PositionSetup/>,
+    document.getElementById('root')
+);
---
+++ packages/router-app/src/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './index.css';
+import { PositionSetup, Game } from 'components'
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Game</Link>
+            </li>
+            <li>
+              <Link to="/setup">Setup</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/setup">
+            <PositionSetup />
+          </Route>
+          <Route path="/game">
+            <Game />
+          </Route>
+          <Route path="/">
+            <Game />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+// function Home() {
+//   return <h2>Home</h2>;
+// }
+
+// function About() {
+//   return <h2>About</h2>;
+// }
+
+// function Users() {
+//   return <h2>Users</h2>;
+// }
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
